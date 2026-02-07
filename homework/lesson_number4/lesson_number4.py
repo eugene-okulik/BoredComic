2c405e0639ba1be68dceef7cdc8dce08cab76598
Alibek Bolatov <alibekbolatovich@icloud.com>
2026-02-07 11:34:59 +0500

    'lesson_number6'

--- homework/lesson_number4/lesson_number4.py
+++ homework/lesson_number4/lesson_number4.py
@@ -1,18 +1,18 @@
-# Task 1
-
-person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
-
-name, last_name, city, phone, country = person
-
-print(name, last_name, city, phone, country)
-
-# Task 2
-
-text = "Здесь написано какой-то текст: 20"
-
-string_index = text.index(':') + 2
-
-print(int(text[string_index:]) + 10)
+# # Task 1
+#
+# person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+#
+# name, last_name, city, phone, country = person
+#
+# print(name, last_name, city, phone, country)
+#
+# # Task 2
+#
+# text = "Здесь написано какой-то текст: 20"
+#
+# string_index = text.index(':') + 2
+#
+# print(int(text[string_index:]) + 10)
 
 # Task 3
 
